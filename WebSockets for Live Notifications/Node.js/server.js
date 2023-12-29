8d8151594fc705a2aea1ce98b1c49e0be1b504ec
Abishek2133 <153486979+Abishek2133@users.noreply.github.com>
2023-12-29 16:00:35 +0530

    Add files via upload

---
+++ WebSockets for Live Notifications/Node.js/server.js
@@ -0,0 +1,15 @@
+// server.js
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('Received: %s', message);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
